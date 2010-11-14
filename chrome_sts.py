629e178b12e8c43078d318a1d64ba521b4540fc8
Ian Gallagher <crash@neg9.org>
2010-11-14 01:24:40 -0800

    Able to load data and add entries (in memory)

--- chrome_sts.py
+++ chrome_sts.py
@@ -7,50 +7,119 @@ import sys
 import struct
 import json
 import hashlib
+import time
+
+debug_levels = {0: 'ERROR',
+                1: 'WARNING',
+                2: 'INFO',
+                3: 'DEBUG'}
+
+DEBUG = 2
 
 class chrome_sts():
-    sts_state_file = os.path.join(os.environ['HOME'], 'Library/Application Support/Google/Chrome/Default/TransportSecurity')
-    sts_state_json = None
+    def __init__(self, sts_state_file=None):
+        # Load the json file into memory
+        sts_state_json = dict()
+        self.sts_entries = dict()
+        
+        try:
+            sts_state_json = json.loads(open(sts_state_file, 'r').read())
+        except Exception, ex:
+            debug(0, "Failed to load STS State file %s (%s)" % (sts_state_file, ex))
+            sys.exit(1)
+            
+        for k, v in sts_state_json.items():
+            entry = sts_entry(k,
+                              created = v['created'],
+                              expiry = v['expiry'],
+                              include_subdomains = v['include_subdomains'],
+                              mode = v['mode']
+                              )
+            self.sts_entries.update(entry.getDict())
 
-    def hostToLabels(self, host):
-        host = host.strip()
-
-        labels = tuple(host.split('.'))
-
-        # Remove any empty labels due to extraneous dots
-        labels = filter(lambda x: len(x) > 0, labels)
-
-        return labels
-
-    def canonicalizeHost(self, host):
-        labels = self.hostToLabels(host)
-
-        canonicalized_host = ''
-        for label in labels:
-            label_len = len(label)
-            canonicalized_host += chr(label_len)
-            canonicalized_host += label
-
-        # Null-terminate
-        canonicalized_host += '\0'
-
-        return canonicalized_host
-
-    def loadSTS(self):
-        self.sts_state_json = json.loads(open(self.sts_state_file, 'r').read())
-
-    def lookup_host(self, host):
-        canonicalized_host = self.canonicalizeHost(host)
-        hashed_host = hashlib.sha256(canonicalized_host).digest().encode('base64').strip()
-        if self.sts_state_json.has_key(hashed_host):
-            return self.sts_state_json[hashed_host]
+    def get(self, host):
+        hashed_host = hashHost(host)
+        debug(3, "Host hash: %s" % hashed_host)
+        if self.sts_entries.has_key(hashed_host):
+            return self.sts_entries[hashed_host]
         else:
             return None
 
+    def add(self, host, max_age=365*24*60*60, include_subdomains=False, mode='strict'):
+        """Add a new entry to the sts object"""
+        hashed_hostname = hashHost(host)
+        cur_time = time.time()
+        cur_time_s = "%.6f" % (cur_time)
+        expiration = "%.6f" % (cur_time + max_age)
+        new_entry = sts_entry(hashed_hostname,
+                              created=cur_time_s,
+                              expiry=expiration,
+                              include_subdomains=include_subdomains,
+                              mode=mode)
+
+        self.sts_entries.update(new_entry)
+        debug(2, "Added/updated STS Entry for %s: %s" % (repr(host), json.dumps(new_entry.getDict())))
+
+class sts_entry(dict):
+    """
+    Class that represents a single entry in the Chrome STS list
+    Example from file:
+        "j6md0fxp6QYNP8B0wy4GhW10q925k2nmAkN+LQsMG6U=": {
+           "created": 1288843191.608916,
+           "expiry": 1320710325.353056,
+           "include_subdomains": false,
+           "mode": "strict"
+        }
+    """
+    def __init__(self, hash, created=None, expiry=None, include_subdomains=False, mode="Strict"):
+        super(sts_entry, self).__init__()
+        self.hash = hash
+        self.created = created
+        self.expiry = expiry
+        self.include_subdomains = include_subdomains
+        self.mode = mode
+
+        # Attributes which will be the key for our hash
+        attributes = {
+            'created': self.created,
+            'expiry': self.expiry,
+            'include_subdomains': self.include_subdomains,
+            'mode': self.mode
+        }
+        
+        # Set ourselves
+        self.__setitem__(hash, attributes)
+
+def canonicalizeHost(host):
+    """Return an RFC3490 compatible canonicalized DNS hostname. Adapated from Scapy source."""
+    temp = [label[:63] for label in host.split(".")] # Truncate labels that cannont be encoded (more than 63 bytes..)
+    temp = map(lambda x: chr(len(x)) + x, temp)
+    temp = ''.join(temp)
+    if temp[-1] != "\x00":
+        temp += "\x00"
+
+    debug(3, "Canonicalized hostname: %s" % repr(temp))
+    return temp
+    
+def hashHost(host):
+    """Generate a hash suitable for use in Chrome STS cache based on a human-readable hostname"""
+    canonicalized_host = canonicalizeHost(host)
+    hashed_host = hashlib.sha256(canonicalized_host).digest()
+    hashed_host = hashed_host.encode('base64').strip()
+    
+    return hashed_host
+    
+def debug(level, msg):
+    """Print message based on debug level"""
+    if DEBUG >= level:
+        print >>sys.stderr, "%s" % debug_levels[level] + ':', msg
+
+
 if '__main__' == __name__:
-    csts = chrome_sts()
-    csts.loadSTS()
-    result = csts.lookup_host(sys.argv[1])
+    path = os.path.join(os.environ['HOME'], 'Library/Application Support/Google/Chrome/Default/TransportSecurity')
+    csts = chrome_sts(sts_state_file=path)
+
+    result = csts.get(sys.argv[1])
     if result:
         print result
     else:
